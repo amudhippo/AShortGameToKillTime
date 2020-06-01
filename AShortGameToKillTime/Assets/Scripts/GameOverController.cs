180257448c84ad8ddbf868f143cde1e4cf4fdd6f
amudhippo <m.ben.oppih@gmail.com>
2020-05-31 23:44:24 -0500

    I'm running out of time. So I'm throwing in what I've got.

--- AShortGameToKillTime/Assets/Scripts/GameOverController.cs
+++ AShortGameToKillTime/Assets/Scripts/GameOverController.cs
@@ -7,16 +7,30 @@ public class GameOverController : MonoBehaviour
     public GameObject player;
     public GameObject LosingGameOverScreen;
     public GameObject WinningGameOverScreen;
+    bool gameOver;
+    float endGameTimer = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver)
+        {
+            endGameTimer -= 1 * Time.deltaTime;
+            print(endGameTimer);
+            if (endGameTimer < 0)
+            {
+                #if UNITY_EDITOR
+                    UnityEditor.EditorApplication.isPlaying = false;
+                #else
+                    Application.Quit();
+                #endif
+            }
+        }
     }
 
     public void GameLost()
@@ -25,6 +39,7 @@ public class GameOverController : MonoBehaviour
         rigidBody.MovePosition(new Vector3(1000, 1000, 1000));
         rigidBody.useGravity = false;
         LosingGameOverScreen.SetActive(true);
+        gameOver = true;
     }
 
     public void GameWon()
@@ -33,7 +48,7 @@ public class GameOverController : MonoBehaviour
         Rigidbody rigidBody = player.GetComponent<Rigidbody>();
         rigidBody.MovePosition(new Vector3(1000, 1000, 1000));
         rigidBody.useGravity = false;
-
         WinningGameOverScreen.SetActive(true);
+        gameOver = true;
     }
 }
